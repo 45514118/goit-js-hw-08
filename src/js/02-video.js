68bfd35147130ebcc0b4603939e1d14d9605087c
Dinar Agzyamov <mail@deenar.ru>
2022-02-04 09:34:50 +0300

    homework 8.2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const currentTime = JSON.parse(localStorage.getItem('videoplayer-current-time')) || 0;
+player.setCurrentTime(currentTime);
+
+const onPlay = throttle(() => {
+  player.getCurrentTime().then(sec => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(sec));
+  });
+}, 1000);
+
+player.on('timeupdate', onPlay);
